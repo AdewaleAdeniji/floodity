d1469437b4722be197a411c00f40bb92aee93c26
nijiferanmi <feranmi@kickgame.co.uk>
2022-10-18 16:18:22 +0100

    Initial commit for floodity

---
+++ map/mapdata.js
@@ -0,0 +1,250 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#02a95c",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    NGA2839: {
+      name: "Borno",
+      description: " "
+    },
+    NGA2841: {
+      name: "Abia",
+      description: " "
+    },
+    NGA2842: {
+      name: "Akwa Ibom",
+      description: " "
+    },
+    NGA2843: {
+      name: "Imo",
+      description: " "
+    },
+    NGA2844: {
+      name: "Rivers",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2845: {
+      name: "Bayelsa",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2846: {
+      name: "Benue",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2847: {
+      name: "Cross River",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2848: {
+      name: "Taraba",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2849: {
+      name: "Kwara",
+      description: " "
+    },
+    NGA2850: {
+      name: "Lagos",
+      description: " "
+    },
+    NGA2851: {
+      name: "Niger",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2852: {
+      name: "Ogun",
+      description: " "
+    },
+    NGA2853: {
+      name: "Ondo",
+      description: " "
+    },
+    NGA2854: {
+      name: "Ekiti",
+      description: " "
+    },
+    NGA2855: {
+      name: "Osun",
+      description: " "
+    },
+    NGA2856: {
+      name: "Oyo",
+      description: " "
+    },
+    NGA2857: {
+      name: "Anambra",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2858: {
+      name: "Bauchi",
+      description: " "
+    },
+    NGA2859: {
+      name: "Gombe",
+      description: " "
+    },
+    NGA2860: {
+      name: "Delta",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2861: {
+      name: "Edo",
+      description: " "
+    },
+    NGA2862: {
+      name: "Enugu",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2863: {
+      name: "Ebonyi",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2864: {
+      name: "Kaduna",
+      description: " "
+    },
+    NGA2865: {
+      name: "Kogi",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2866: {
+      name: "Plateau",
+      description: " "
+    },
+    NGA2867: {
+      name: "Nassarawa",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2868: {
+      name: "Jigawa",
+      description: " "
+    },
+    NGA2869: {
+      name: "Kano",
+      description: " "
+    },
+    NGA2870: {
+      name: "Katsina",
+      description: " "
+    },
+    NGA2871: {
+      name: "Sokoto",
+      description: " "
+    },
+    NGA2872: {
+      name: "Zamfara",
+      description: " "
+    },
+    NGA2873: {
+      name: "Yobe",
+      description: " "
+    },
+    NGA2879: {
+      name: "Kebbi",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA2881: {
+      name: "Adamawa",
+      description: " ",
+      color: "#FF0000"
+    },
+    NGA3470: {
+      name: "Federal Capital Territory",
+      description: " "
+    }
+  },
+  locations: {
+    "0": {
+      lat: "9.083333",
+      lng: "7.533333",
+      name: "Abuja"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
