fd757057e39c3cf7ffe6fbb7e05dd812dcb2ac3e
nijiferanmi <feranmi@kickgame.co.uk>
2022-10-19 08:18:41 +0100

    Initial commit for floodity

--- map/mapdata.js
+++ map/mapdata.js
@@ -1,7 +1,7 @@
 var simplemaps_countrymap_mapdata={
   main_settings: {
    //General settings
-    width: "300", //'700' or 'responsive'
+    width: "responsive", //'700' or 'responsive'
     background_color: "#FFFFFF",
     background_transparent: "yes",
     border_color: "#ffffff",
@@ -9,7 +9,7 @@ var simplemaps_countrymap_mapdata={
     //State defaults
     state_description: "State description",
     state_color: "#88A4BC",
-    state_hover_color: "#02a95c",
+    state_hover_color: "#3B729F",
     state_url: "",
     border_size: 1.5,
     all_states_inactive: "no",
@@ -90,17 +90,16 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2844: {
       name: "Rivers",
-      description: " ",
-      color: "#FF0000"
+      color: "#FF0000",
+      description: "In Rivers State which is expected to bear the final brunt of the flooding as they host the last channels for the water to empty into the sea, four local governments have so far been impacted, added to rising sea levels due to the melting of the ice cap, they are expected to be more impacted what with the prediction that more rains and flooding should be expected but so far, there have not been damning results of the flooding in the state."
     },
     NGA2845: {
       name: "Bayelsa",
-      description: " ",
       color: "#FF0000"
     },
     NGA2846: {
       name: "Benue",
-      description: " ",
+      description: "Benue State which has had the unfortunate problem of witnessing the flooding yearly as the Benue River is always a natural course down to River Niger to the ocean had the double misfortune of higher volume of rains and the swelling banks of the Benue River. The result was the flooding of residential areas with bungalows disappearing in the swollen banks of the river. Those who had gone to sleep before the river burst its banks simply died in their sleep as they were submerged. Worst hit were women and children.\n\nThe casualty figure in Benue State was put at 23 dead and 76 injured while over 100 communities were adversely affected which they said would have a telling effect on the agrarian state referred to as the food basket of the nation. Farmlands were not spared where over 50 per cent of the population engage in agriculture. Their means of livelihood were simply wiped out. In the Makurdi metropolis, rising water level got as high as roofs, forcing residents to flee. Nigeria Air Force Base, Kilometre Five, Gyado Villa, Wurukum, Kucha Utebe, Wadata and timber market were worst hit among others.",
       color: "#FF0000"
     },
     NGA2847: {
@@ -110,12 +109,13 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2848: {
       name: "Taraba",
-      description: " ",
+      description: "In Taraba State which is also largely agrarian, the flood left in its wake tales of woes as farmers lost a season harvest as well as newly planted crops. Houses were not spared but their luck was that the level receded within days to enable them start recovering from the tragedy. It however made refugees of most of those living in lowlands",
       color: "#FF0000"
     },
     NGA2849: {
       name: "Kwara",
-      description: " "
+      description: "Kwara which is contiguous to Niger State recorded four deaths where the flashfloods swept a vehicle into the river. Farmlands and residential houses were affected while some bridges linking important places were washed away. It was so serious that public affairs almost ground to a halt as people feared being washed away by floods and rising water level.",
+      color: "#ff0000"
     },
     NGA2850: {
       name: "Lagos",
@@ -123,7 +123,7 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2851: {
       name: "Niger",
-      description: " ",
+      description: "In Niger State, authorities listed Rafi, Kontagora, Lavun, Wushishi, Mashegu, Magama and Gbako as the most impacted Local government which at the last count, no fewer than eight persons were confirmed dead while culverts, roads and bridges were washed away. People whose homesteads were affected have been camped in public buildings while those with relatives that could harbour them have moved to such places.",
       color: "#FF0000"
     },
     NGA2852: {
@@ -148,20 +148,24 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2857: {
       name: "Anambra",
-      description: " ",
-      color: "#FF0000"
+      description: "Areas mostly impacted in Anambra aside Ogbaru and Onitsha North are Onitsha South, Anambra East, Anambra West, Awka North, Ayamelum and Oyi local government areas.",
+      color: "#FF0000",
+      image_url: "https://pbs.twimg.com/media/FfZKrOsXkAIk_8s?format=jpg&name=small",
+      url: "https://pbs.twimg.com/media/FfZKrOsXkAIk_8s?format=jpg&name=small"
     },
     NGA2858: {
       name: "Bauchi",
-      description: " "
+      description: "Bauchi State where casualty figures was put at three while 1450 houses were damaged. Farmlands and home appliances were also among part of the losses recorded in the state.",
+      color: "#ff0000"
     },
     NGA2859: {
       name: "Gombe",
-      description: " "
+      description: "Gombe figures of displaced people were low with authorities quoting 500 though lamentations were rife over loss of farmlands and personal effects.",
+      color: "#ff0000"
     },
     NGA2860: {
       name: "Delta",
-      description: " ",
+      description: "44,099 houses are partially damaged; 45,249 houses totally damaged; 76,168 hectares of farmland partially damaged and 70,566 hectares of farmland are completely destroyed by the great deluge",
       color: "#FF0000"
     },
     NGA2861: {
@@ -184,8 +188,8 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2865: {
       name: "Kogi",
-      description: " ",
-      color: "#FF0000"
+      color: "#FF0000",
+      description: "Ibaji Kogi State is almost 100 per cent under water while the rest range from 30 per cent up"
     },
     NGA2866: {
       name: "Plateau",
@@ -193,7 +197,7 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2867: {
       name: "Nassarawa",
-      description: " ",
+      description: "In neighbouring Nasarawa state, which is also grappling with flood water cascading down the River Benue, farmers are counting their losses from ravaged farmlands.",
       color: "#FF0000"
     },
     NGA2868: {
@@ -206,7 +210,8 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2870: {
       name: "Katsina",
-      description: " "
+      description: "In Katsina, windstorm and flooding combined to wreak an unprecedented havoc in the state which spread across 34 local government areas with death toll between 24 and 42. Officials said 16,625 houses were destroyed while 1,620 farmlands were submerged in at least three council areas of Kafur, Danja and Ingawa.",
+      color: "#ff0000"
     },
     NGA2871: {
       name: "Sokoto",
@@ -227,8 +232,8 @@ var simplemaps_countrymap_mapdata={
     },
     NGA2881: {
       name: "Adamawa",
-      description: " ",
-      color: "#FF0000"
+      color: "#FF0000",
+      description: "North-eastern Adamawa, where more people are dying from flood-related incidents."
     },
     NGA3470: {
       name: "Federal Capital Territory",
